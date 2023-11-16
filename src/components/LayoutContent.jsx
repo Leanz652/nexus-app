a6d6958ce0dfc5d5f4b508e023ce45c318146fc1
Leanz652 <lean.alvarez@live.com.ar>
2023-11-15 21:15:05 -0300

    upd screen

--- src/components/LayoutContent.jsx
+++ src/components/LayoutContent.jsx
@@ -5,15 +5,15 @@ import Footer  from './Footer';
 /* eslint-disable react/prop-types */
 const LayoutContent = ({ children, imageUrl }) => {
   return (
-    <div className="flex-col mt-[98px] sm:mt-0 flex h-full px-10 sm:px-20 items-center">
+    <div className="flex-col pt-[98px] sm:mt-0 flex min-h-full h-auto px-10 sm:px-20 items-center">
       <div className="flex flex-col flex-1 items-center  sm:flex-row  ">
         <div className="sm:w-1/2 sm:h-3/4">{children}</div>
-        <div className="sm:w-1/2 sm:text-right sm:flex sm:flex-row-reverse sm:translate-y-10 my-5 sm:m-0">
+        <div className="sm:w-1/2 sm:text-right sm:flex sm:flex-row-reverse my-5 sm:m-0">
           <img src={imageUrl} alt="Imagen"/>
         </div>
       </div>
       
-      <div className=' mt-auto flex-none w-full '>
+      <div className=' mt-auto flex-none w-full pt-5'>
       <Footer />
       </div>
 
