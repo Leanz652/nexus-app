a2773f288e80630cb596438e8a0b3c7f1343be70
Leanz652 <lean.alvarez@live.com.ar>
2023-11-17 11:08:31 -0300

    upd

--- src/components/LayoutContent.jsx
+++ src/components/LayoutContent.jsx
@@ -8,8 +8,8 @@ const LayoutContent = ({ children, imageUrl }) => {
     <div className="flex-col pt-[98px] sm:mt-0 flex min-h-full h-auto px-10 sm:px-20 items-center">
       <div className="flex flex-col flex-1 items-center  sm:flex-row  ">
         <div className='flex flex-col sm:flex-row sm:h-3/4' >
-        <div className="sm:w-1/2">{children}</div>
-        <div className="flex mt-2 sm:mt-0 align-top justify-end ">
+        <div className="sm:w-2/3">{children}</div>
+        <div className="flex mt-2 sm:mt-0 items-center justify-end ">
           <img src={imageUrl} alt="Imagen" className='sm:h-[85%] ' />
         </div>
         </div>
